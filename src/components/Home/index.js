eb690e0c3ca9761540ce1f236b166c4912bc4de9
1chary <abhilashchary04@gmail.com>
2024-05-18 14:11:16 +0530

    welcome page

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+const Home = () => (
+        <div className = "container">
+          <h1 className = "headingOfTheWelcomePage">
+            WELCOME TO THE HOME COMPONENT
+          </h1>
+        </div>
+)
+
+export default Home
