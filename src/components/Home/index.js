bb51f7e041a7adcebfbfecf113eded3a322d0ce7
1chary <abhilashchary04@gmail.com>
2024-06-11 12:04:24 +0530

    fetching data with url

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,27 @@
+import {useEffect} from "react"
+import { FaHome } from "react-icons/fa";
 import "./index.css"
 
-const Home = () => (
-        <div className = "container">
-          <h1 className = "headingOfTheWelcomePage">
-            WELCOME TO THE HOME COMPONENT
-          </h1>
-        </div>
-)
+
+
+const Home = () => {
+  
+  useEffect(() => {
+    const fetchAllTheMovies = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=3ebbee02535b2c5e2a5646788e3b6384&language=en-US&page=1');
+      const data = await response.json()
+      console.log(data)
+    }
+    
+    fetchAllTheMovies()
+  })
+
+  return (
+    <div>
+      <FaHome />
+      <h1>Hi</h1>
+    </div>
+  )
+}
 
 export default Home
